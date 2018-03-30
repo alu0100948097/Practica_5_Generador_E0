3aaaea49061dc3934815d428a4f1183888513247
alu0100948097 <alu0100948097@ull.edu.es>
2018-03-30 13:35:00 +0100

    Declaración método XOR

--- lib/e0.rb
+++ lib/e0.rb
@@ -18,5 +18,10 @@ class E0
         @i_c=0
 	end
     
+    def XOR(a,b)
+        temp=a.unpack('C*').zip(b.unpack('C*'))
+        temp=temp.map {|x,y| x^y}.join
+        return temp
+    end
     
 end
