2f06a641a79eeef17d8ad0ccd9a7c27d9d8b3656
alu0100948097 <alu0100948097@ull.edu.es>
2018-03-30 13:34:42 +0100

    Declaración de atributos

--- lib/e0.rb
+++ lib/e0.rb
@@ -1,1 +1,22 @@
 require 'colorize'
+
+class E0
+    
+    attr_writer :primer, :segundo, :tercer, :cuarto, :r1
+	
+    def initialize
+		@primer=""
+        @segundo=""
+        @tercer=""
+        @cuarto=""
+        @r1=""
+        @bits_salida=""
+        @salida=""
+        @i_p=0
+        @i_s=0
+        @i_t=0
+        @i_c=0
+	end
+    
+    
+end
